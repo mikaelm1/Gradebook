9d0e5b56a1d6029f490e81c879851ed49925dd64
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-04-03 07:43:07 -0700

    feat: Draft UI for Login screen

---
+++ Gradebook/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVC.swift
+//  Gradebook
+//
+//  Created by Mikael Mukhsikaroyan on 4/3/16.
+//  Copyright © 2016 msquared. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
