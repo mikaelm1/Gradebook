71c0b525dbd345be064f2ce6f7756f1fe2495a8a
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2016-04-04 11:00:06 -0700

    feat: Fetched results controller delegate and methods added to the class

--- Gradebook/ClassesTableVC.swift
+++ Gradebook/ClassesTableVC.swift
@@ -9,24 +9,38 @@
 import UIKit
 import CoreData
 
-class ClassesTableVC: UITableViewController {
+class ClassesTableVC: UITableViewController, NSFetchedResultsControllerDelegate {
     
     var sharedContext: NSManagedObjectContext {
         return CoreDataStackManager.sharedInstance().managedObjectContext
     }
+    
+    lazy var fetchedResultsController: NSFetchedResultsController = {
+        let fetchRequest = NSFetchRequest(entityName: "Course")
+        fetchRequest.sortDescriptors = []
+        //print("FetchRequest; \(fetchRequest)")
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.sharedContext, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController.delegate = self
+        return fetchedResultsController
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
+        setUpUI()
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            print("Unable to perform fethc")
+        }
+        
+    }
+    
+    func setUpUI() {
         let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(ClassesTableVC.addButtonPressed))
         self.navigationItem.rightBarButtonItem = addButton
         self.title = "Classes"
-        
-        sharedContext 
-        
     }
 
     func addButtonPressed() {
@@ -37,11 +51,12 @@ class ClassesTableVC: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return fetchedResultsController.sections?.count ?? 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        let sectionInfo = fetchedResultsController.sections![section]
+        return sectionInfo.numberOfObjects
     }
 
     
@@ -53,19 +68,65 @@ class ClassesTableVC: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
+        print("Selected cell at index \(indexPath)")
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - Fetched Results Controller Delegate
+    
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        tableView.beginUpdates()
     }
-    */
+    
+    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+        
+        switch type {
+        case .Insert:
+            print("Inserted section")
+        case .Delete:
+            print("Deleted section")
+        default:
+            print("Section was changed in default switch case")
+        }
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+        
+        switch type {
+        case .Insert:
+            tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+            
+        case .Delete:
+            tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+            
+        case .Update:
+            let cell = tableView.cellForRowAtIndexPath(indexPath!)
+            let course = controller.objectAtIndexPath(indexPath!)
+            // TODO: configure the cell with the new course 
+            
+        case .Move:
+            tableView.deleteRowsAtIndexPaths([indexPath!], withRowAnimation: .Fade)
+            tableView.insertRowsAtIndexPaths([newIndexPath!], withRowAnimation: .Fade)
+        }
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        tableView.endUpdates()
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
